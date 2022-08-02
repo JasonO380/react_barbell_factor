9b2ebb7cb3e326b710d5200399da27e96deba0da
Jason Ollada <JMOllada@gmail.com>
2022-08-02 13:20:44 -0500

    Updates

---
+++ precache-manifest.aa657666a336784f27055b35399ca82c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b71277bd0063ddc488a",
+    "url": "/Barbell_Factor_React/static/css/main.466f4262.chunk.css"
+  },
+  {
+    "revision": "4b71277bd0063ddc488a",
+    "url": "/Barbell_Factor_React/static/js/main.4b71277b.chunk.js"
+  },
+  {
+    "revision": "ac5b1bae6ab4fa649ed7",
+    "url": "/Barbell_Factor_React/static/js/1.ac5b1bae.chunk.js"
+  },
+  {
+    "revision": "8a6914aa9e84f088caf3",
+    "url": "/Barbell_Factor_React/static/js/2.8a6914aa.chunk.js"
+  },
+  {
+    "revision": "a88c8e313613a5ab85f8",
+    "url": "/Barbell_Factor_React/static/js/runtime~main.a88c8e31.js"
+  },
+  {
+    "revision": "ec8a2badf189b6f51d35797aa8ed79db",
+    "url": "/Barbell_Factor_React/static/media/barbellfactor.ec8a2bad.jpeg"
+  },
+  {
+    "revision": "c2e0092361af3c5c7b92d52c437d1303",
+    "url": "/Barbell_Factor_React/static/media/profilepic.c2e00923.jpeg"
+  },
+  {
+    "revision": "7a59e257704bec1df5247c8559419a8b",
+    "url": "/Barbell_Factor_React/index.html"
+  }
+];
